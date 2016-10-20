facba29efab3c9c4dbfd605302f1abfdd8116adb
sebizs@gmail.com <sebizs@gmail.com>
2016-10-20 21:48:33 +0200

    Changes to be committed: 	new file:   main.java 	new file:   test.file

---
+++ main.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.*;
+
+public class main {
+
+	public static int ListenPort=5514;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
+
+		ServerSocket echoServer = null;
+        String line;
+        DataInputStream is;
+        Socket clientSocket = null;
+        try {
+            echoServer = new ServerSocket(ListenPort);
+         }
+         catch (IOException e) {
+            System.out.println(e);
+         }   
+        try {
+            clientSocket = echoServer.accept();
+            is = new DataInputStream(clientSocket.getInputStream());
+
+            while (true) {
+              line = is.readLine();
+              if (line != null)
+              {
+            	  System.out.println(line);
+            	}
+              else
+              {
+            	  try {
+            	       is.close();
+            	       echoServer.close();
+            	    } 
+            	    catch (IOException e) {
+            	       System.out.println(e);
+            	    }
+            	  echoServer = new ServerSocket(ListenPort);
+            	  clientSocket = echoServer.accept();
+                  is = new DataInputStream(clientSocket.getInputStream());
+            	  System.out.println(ListenPort);
+              }
+            }
+         }   
+     catch (IOException e) {
+            System.out.println(e);
+         }
+	}
+}
+

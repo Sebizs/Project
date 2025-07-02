e46aa8f87fdc08e6266a3427c9c10e6a801e6843
Sebizs <sebizs@gmail.com>
2025-07-02 13:33:19 +0200

    Create RegexpPattern.java
    add parser

---
+++ RegexpPattern.java
@@ -0,0 +1,11 @@
+public class RegexpPattern {
+    public int id;
+    public String name;
+    public String pattern;
+
+    public RegexpPattern(int id, String name, String pattern) {
+        this.id = id;
+        this.name = name;
+        this.pattern = pattern;
+    }
+}

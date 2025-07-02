58663200c22702055d865b62d75f52b542746e0e
Sebizs <sebizs@gmail.com>
2025-07-02 13:08:01 +0200

    Create ServerConfig.java
    modularity

---
+++ ServerConfig.java
@@ -0,0 +1,35 @@
+// ServerConfig.java
+
+public class ServerConfig {
+    public int listenPort;
+    public boolean sslEnabled;
+    public String sslKeystorePath;
+    public String sslKeystorePassword;
+    public int socketTimeoutMs;
+    public int threadPoolSize;
+    public String storageBackend;
+    public MySQLConfig mysql;
+    public ElasticsearchConfig elasticsearch;
+    public ForwardingConfig forwarding;
+
+    public static class MySQLConfig {
+        public String host;
+        public int port;
+        public String database;
+        public String user;
+        public String password;
+    }
+
+    public static class ElasticsearchConfig {
+        public String host;
+        public int port;
+        public String index;
+    }
+
+    public static class ForwardingConfig {
+        public boolean enabled;
+        public String host;
+        public int port;
+        public boolean useTls;
+    }
+} 
